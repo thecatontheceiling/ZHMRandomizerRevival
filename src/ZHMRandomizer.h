45b56985a0b5fdfd4c4de996f874fdbdd4e9bcca
Lyssa <emailforstuffz9876@gmail.com>
2025-08-31 13:47:13 +0400

    sigh

--- src/ZHMRandomizer.h
+++ src/ZHMRandomizer.h
@@ -1,8 +1,16 @@
 #pragma once
 
 #include <IPluginInterface.h>
-
 #include <Glacier/SGameUpdateEvent.h>
+#include <memory>
+#include "randomizers/Randomizer.h"
+
+namespace hitman_randomizer {
+    class Config;
+    class RandomDrawRepository;
+    class DefaultItemPoolRepository;
+    class Randomizer;
+}
 
 class ZHMRandomizer : public IPluginInterface {
 public:
@@ -12,6 +20,22 @@ public:
     void OnDrawUI(bool p_HasFocus) override;
 
 private:
+    std::shared_ptr<hitman_randomizer::RandomDrawRepository> m_repo;
+    std::unique_ptr<hitman_randomizer::DefaultItemPoolRepository> m_defaultItemPoolRepo;
+
+    std::unique_ptr<hitman_randomizer::Randomizer> m_worldInventoryRandomizer;
+    std::unique_ptr<hitman_randomizer::Randomizer> m_npcItemRandomizer;
+    std::unique_ptr<hitman_randomizer::Randomizer> m_heroInventoryRandomizer;
+    std::unique_ptr<hitman_randomizer::Randomizer> m_stashItemRandomizer;
+
+    std::shared_ptr<hitman_randomizer::Config> m_config;
+    std::shared_ptr<hitman_randomizer::RandomDrawRepository> m_repo;
+    std::unique_ptr<hitman_randomizer::DefaultItemPoolRepository> m_defaultItemPoolRepo;
+
+    void createRandomizers();
+
+    bool m_isMenuVisible = false;
+
     void OnFrameUpdate(const SGameUpdateEvent& p_UpdateEvent);
     DECLARE_PLUGIN_DETOUR(ZHMRandomizer, void, OnLoadScene, ZEntitySceneContext* th, ZSceneData& p_SceneData);
 
