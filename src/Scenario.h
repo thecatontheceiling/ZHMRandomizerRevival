d061ffb99c5dc1ba6b3a8788c04a74460d8118b3
Lyssa <emailforstuffz9876@gmail.com>
2025-08-31 13:04:15 +0400

    get rid of trash

--- src/Scenario.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ZHM5RANDOMIZER_SRC_SCENARIO_H__
-#define __ZHM5RANDOMIZER_SRC_SCENARIO_H__
-
-#include "SSceneInitParameters.h"
-
-using Scenario = uint64_t;
-
-#endif // __ZHM5RANDOMIZER_SRC_SCENARIO_H__
